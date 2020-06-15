639c08cd4bdb4a663dd83fb12b0889e516ef2b68
Sofie Weetink <sofie.weetink@coderclass.nl>
2020-06-15 12:12:47 +0200

    Uitgebreider
    Ik heb een menu gemaakt
    Ik heb keuzes toegevoegd

--- contacten.py
+++ contacten.py
@@ -1,40 +1,61 @@
 def print_menu(naam):
-    #hier komt het menu in te staat
-    pass
+    #Hier komt het menu in te staat.
+    print("Welkom " + naam + ", hier kun je je contacten aanpassen/opslaan/bekijken/aanmaken. \n\nVoor aanpassen kies 'a'\nVoor opslaan kies 'o'\nVoor het bekijken van al je contacten kies 'b'\nVoor het aanmaken van contacten kies 'n' \nAls je er geen zin meer in hebt kies dan 'q'\n")
 
 def main():
     #Dit is de main van het programma dus waar alle andere functies worden aan geroepen.
     doorgaan = True
-    contacten = {"Piet": 12345678, "Willie": 87654321, "Jantje": 23456789}#dit zijn even random nummers ik voeg later echte namen en nummers toe 
+    contacten = {"Piet": 12345678, "Willie": 87654321, "Jantje": 23456789}#dit zijn even random nummers ik voeg later echte namen en nummers toe. 
     naam = input("Hoe heet je: ")
     while doorgaan:
-        #in de loop gaat alles gebeuren
+        #In de loop gaat alles gebeuren.
         print_menu(naam)
-        keuze = input("Wat is je keuze geworden: ")
+        keuze = input("Wat is je keuze geworden (a/o/b/n/q): ")
+        if keuze == "a":
+            contact_aanpassen(contacten)
+            
+        elif keuze == "o":
+            contacten_opslaan(contacten)
+            
+        elif keuze == "b":
+            bekijk_contacten(contacten)
+            
+        elif keuze == "n":
+            contacten_toevoegen(contacten)
+            
+        elif keuze == "q":
+            doorgaan = False
+             
+        else:
+            print("Je hebt iets verkeerts getypt probeer het nog een keer, ik laat je de mogelijkheden nog een keer zien.")
 
-def contact_toevoegen():
-    #Hier komt de code voor als je er voor kiest om een contact toe te voegen
-    pass
+def contact_toevoegen(contacten):
+    #Hier komt de code voor als je er voor kiest om een contact toe te voegen.
+    print (contacten)
     
-def lijst_van_contacten():
-    #Hier komt de code voor als je er voor kiest om al je contacten en hoeveel je er hebt te bekijken
-    pass
-def toon_contacten():
-    #voor het laten zien van al je contacten
-    pass
-def aantal_contacten():
-    #voor het kijken naar hoeveel contacten je hebt
-    pass
+def bekijk_contacten(contacten):
+    #Hier komt de code voor als je er voor kiest om al je contacten en hoeveel je er hebt te bekijken.
+    print (contacten)
+            
+def toon_aantal_contacten(contacten):
+    #Voor het laten zien van al je contacten.
+    print (contacten)
+            
+def aantal_contacten(contacten):
+    #Voor het kijken naar hoeveel contacten je hebt.
+    print (contacten)
 
-def contact_verwijderen():
+def contact_verwijderen(contacten):
     #Hier komt de code voor als je er voor kiest om een contact te verwijderen
-    pass
+    print (contacten)
     
-def contact_aanpassen():
+def contact_aanpassen(contacten):
     #Hier komt de code voor als je er voor kiest om een contact aan te passen
+    print (contacten)
+            
     
-def contacten_opslaan():
+def contacten_opslaan(contacten):
     #Hier komt de code voor als je er voor kiest om een contact op te slaan
-    pass
+    print (contacten)
     
-main()
+main()#Dit roept de eerste functie aan
